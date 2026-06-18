dfdb0ce51c034ccbf562b7b7aace0fff230af06c
MGJT <1282261308@@qq.com>
2026-06-18 09:35:02 +0800

    release: v1.5.5

--- src/utils/alova.ts
+++ src/utils/alova.ts
@@ -1,7 +1,7 @@
 import { createAlova } from "alova";
 import adapterFetch from "alova/fetch";
 
-export const baseURL = "http://111.231.18.118:5000";
+export const baseURL = "http://47.99.115.172:5000";
 // export const baseURL = "http://192.168.110.225:5000";
 
 export const alovaInstance = createAlova({
